99beea8ded52d6bc8d38b620c1a011c42b303950
Ben Tran <46247492+BenOnSocial@users.noreply.github.com>
2022-10-01 23:09:28 -0700

    Fixed failures in unit tests where there is no Yelp API key set.

--- src/yelpfusion3/settings.py
+++ src/yelpfusion3/settings.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    api_key: str = Field(..., env="YELP_API_KEY")
+    api_key: Optional[str] = Field(..., env="YELP_API_KEY")
     base_url: str = "https://api.yelp.com/v3"
 
     @property
