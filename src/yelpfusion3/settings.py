bb39ae213e9c1b19c5e1e08fd14847be73e2e0fa
Ben Tran <46247492+BenOnSocial@users.noreply.github.com>
2022-10-03 13:47:19 -0700

    Use Pydantic HttpUrl field to validate base_url value.

--- src/yelpfusion3/settings.py
+++ src/yelpfusion3/settings.py
@@ -1,11 +1,11 @@
 from typing import Optional
 
-from pydantic import BaseSettings, Field
+from pydantic import BaseSettings, Field, HttpUrl
 
 
 class Settings(BaseSettings):
     api_key: Optional[str] = Field(default=None, env="YELP_API_KEY")
-    base_url: str = "https://api.yelp.com/v3"
+    base_url: HttpUrl = "https://api.yelp.com/v3"
 
     @property
     def headers(self) -> dict:
