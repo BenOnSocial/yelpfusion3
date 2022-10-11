3e9034c45ee0b7d0c73066c0a1a7ccd3af616150
Ben Tran <46247492+BenOnSocial@users.noreply.github.com>
2022-10-11 00:19:31 -0700

    Moved Location model to yelpfusion3.model module so it can be reused by other submodules.

--- src/yelpfusion3/business/model.py
+++ src/yelpfusion3/business/model.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 from typing import List, Literal, Optional
 
-import pycountry
 from pydantic import HttpUrl, NonNegativeInt, confloat, constr, validator
 
-from yelpfusion3.model import Model
+from yelpfusion3.model import Location, Model
 
 
 class Category(Model):
@@ -37,74 +36,6 @@ class Coordinates(Model):
     """
     The longitude of this business.
     """
-
-
-class Location(Model):
-    """
-    The location of this business, including address, city, state, zip code and country.
-    """
-
-    address1: Optional[constr(strip_whitespace=True)] = None
-    """
-    Street address of this business.
-    """
-
-    address2: Optional[constr(strip_whitespace=True, min_length=0)] = None
-    """
-    Street address of this business, continued.
-    """
-
-    address3: Optional[constr(strip_whitespace=True, min_length=0)] = None
-    """
-    Street address of this business, continued.
-    """
-
-    city: str
-    """
-    City of this business.
-    """
-
-    zip_code: str
-    """
-    Zip code of this business.
-    """
-
-    country: str
-    """
-    ISO 3166-1 alpha-2 country code of this business.
-    """
-
-    state: str
-    """
-    ISO 3166-2 (with a few exceptions) state code of this business.
-    """
-
-    display_address: List[str]
-    """
-    Array of strings that if organized vertically give an address that is in the standard address format for the
-    business's country.
-    """
-
-    cross_streets: Optional[constr(strip_whitespace=True, min_length=0)] = None
-    """
-    Cross streets for this business. (Only used in business details search results.)
-    """
-
-    @validator("country")
-    def _check_country(cls, v: str) -> str:
-        """
-        Checks that ``v`` is a valid ISO 3166- alpha-2 country code.
-
-        :param v: Two-letter country code.
-        :type v: str
-        :raise ValueError: If ``v`` is an invalid country code.
-        :return: ``v`` if it passes validation.
-        :rtype: str
-        """
-
-        if pycountry.countries.get(alpha_2=v):
-            return v
-        raise ValueError("Not a valid ISO 3166-1 alpha-2 country code.")
 
 
 class Business(Model):
--- src/yelpfusion3/model.py
+++ src/yelpfusion3/model.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+from typing import List, Optional
+
+import pycountry
+from pydantic import BaseModel, constr, validator
 
 
 class Model(BaseModel):
@@ -10,3 +13,71 @@ class Model(BaseModel):
         anystr_strip_whitespace = True
         min_anystr_length = 1
         validate_assignment = True
+
+
+class Location(Model):
+    """
+    The location of a business or event, including address, city, state, zip code and country.
+    """
+
+    address1: Optional[constr(strip_whitespace=True)] = None
+    """
+    Street address of this business or event.
+    """
+
+    address2: Optional[constr(strip_whitespace=True, min_length=0)] = None
+    """
+    Street address of this business or event, continued.
+    """
+
+    address3: Optional[constr(strip_whitespace=True, min_length=0)] = None
+    """
+    Street address of this business or event, continued.
+    """
+
+    city: str
+    """
+    City of this business or event.
+    """
+
+    state: str
+    """
+    ISO 3166-2 (with a few exceptions) state code of this business or event.
+    """
+
+    zip_code: str
+    """
+    Zip code of this business or event.
+    """
+
+    country: str
+    """
+    ISO 3166-1 alpha-2 country code of this business or event.
+    """
+
+    display_address: List[str]
+    """
+    Array of strings that if organized vertically give an address that is in the standard address format for the
+    business or event's country.
+    """
+
+    cross_streets: Optional[constr(strip_whitespace=True, min_length=0)] = None
+    """
+    Cross streets for this business or event. (Only used in business or event details search results.)
+    """
+
+    @validator("country")
+    def _check_country(cls, v: str) -> str:
+        """
+        Checks that ``v`` is a valid ISO 3166- alpha-2 country code.
+
+        :param v: Two-letter country code.
+        :type v: str
+        :raise ValueError: If ``v`` is an invalid country code.
+        :return: ``v`` if it passes validation.
+        :rtype: str
+        """
+
+        if pycountry.countries.get(alpha_2=v):
+            return v
+        raise ValueError("Not a valid ISO 3166-1 alpha-2 country code.")
