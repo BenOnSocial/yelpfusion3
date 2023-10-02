a245a02145bd9bc9f9144a489bc2a5b475b38e90
BenOnSocial <46247492+BenOnSocial@users.noreply.github.com>
2023-10-02 16:35:40 -0700

    Resolved breaking change in validators 0.21.0 that changed the API for between().

--- src/yelpfusion3/business/endpoint.py
+++ src/yelpfusion3/business/endpoint.py
@@ -317,7 +317,7 @@ class BusinessSearchEndpoint(Endpoint):
     def _check_price(cls, value: str) -> str:  # pylint: disable=E0213
         if value and value.strip():
             levels: List[str] = [level.strip() for level in value.split(sep=",")]
-            if all(validators.between(value=int(level), min=1, max=4) for level in levels):
+            if all(validators.between(int(level), min_val=1, max_val=4) for level in levels):
                 return ",".join(levels)
         raise ValueError("Malformed 'price' value.")
 
